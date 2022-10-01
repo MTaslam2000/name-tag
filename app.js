1fc0ebc1df690fbdaa557d1dfc87f49f8ebe867e
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2022-10-01 17:40:03 -0500

    Js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // grab DOM elements
 
 // set event listeners
+
+
+const nameDisplay = document.getElementById('name');
+console.log(nameDisplay);
+
+nameDisplay.textContent ='Julie';
+
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () =>{
+    console.log('i am clicking the button');
+// grab the input element
+const nameInput = document.getElementById('name-input');
+// set the display to the value from the input
+nameDisplay.textContent = nameInput.value;
+});
